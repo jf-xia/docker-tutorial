e14e1edc35af31afd92890fe22ea67395db6e671
Jack <jackxia5@foxmail.com>
2019-01-09 19:12:06 +0800

    Update sonar-scanner.sh

--- sonar/sonar-scanner.sh
+++ sonar/sonar-scanner.sh
@@ -1,3 +1,3 @@
 sonar-scanner -Dsonar.sources=.  -Dsonar.host.url=http://localhost:9000 -Dsonar.exclusions=./node_modules -Dsonar.login=admin -Dsonar.password=admin -Dsonar.projectKey=$(basename `pwd`)
 #sonar.exclusions=./vendor node_modules
- 
+#https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner
