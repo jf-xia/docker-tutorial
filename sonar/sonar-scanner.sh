711c678ef9843674a66747847f4bb70a9e3ef8b0
Jack <jackxia5@foxmail.com>
2019-01-09 19:02:45 +0800

    Update sonar-scanner.sh

--- sonar/sonar-scanner.sh
+++ sonar/sonar-scanner.sh
@@ -1,1 +1,3 @@
-sonar-scanner -Dsonar.sources=.  -Dsonar.host.url=http://localhost:9000 -Dsonar.login=admin -Dsonar.password=admin -Dsonar.projectKey=$(basename `pwd`)
+sonar-scanner -Dsonar.sources=.  -Dsonar.host.url=http://localhost:9000 -Dsonar.exclusions=./node_modules -Dsonar.login=admin -Dsonar.password=admin -Dsonar.projectKey=$(basename `pwd`)
+#sonar.exclusions=./vendor node_modules
+ 
